6d053d9afe3d94c67ed7afaad824705cbd7676e4
Bikash <35362932+explorer855@users.noreply.github.com>
2025-07-31 00:08:02 +0530

    Added masstransit support, did some initial order project setup (#29)

--- src/OrdersApi/Program.cs
+++ src/OrdersApi/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -8,6 +10,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Plugin MassTransit
+
+builder.Services.AddMassTransit(options =>
+{
+    options.SetKebabCaseEndpointNameFormatter();
+
+    options.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(new Uri(builder.Configuration["RabbitMQ:Host"]!), host =>
+        {
+            host.Username(builder.Configuration["RabbitMQ:Username"]!);
+            host.Password(builder.Configuration["RabbitMQ:Password"]!);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+#endregion
 
 var app = builder.Build();
 
